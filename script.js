c4588808db8d5bea1c49e1a98305cd06ce5318e4
Brubi25 <148341586+Brubi25@users.noreply.github.com>
2024-01-31 16:54:24 +0100

    subheads

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ let sub = document.getElementById("sub");
 
 for(let n = 0; n < content_children.length; n++){
 	let child = content_children[n];
-	if (child.tagName == "H2"){
+	if (child.tagName == "H2" || child.tagName == "H3"){
 		const node = document.createElement("li");
 		const textnode = document.createTextNode(child.innerText);
-		node.appendChild(textnode);
 		
+		if(child.tagName == "H3"){
+			node.className = "listsmall";
+		}
+		if(child.tagName == "H2"){
+			node.className = "list";
+		}
+		
+		node.appendChild(textnode);
+
 		node.addEventListener("click", function(){child.scrollIntoView()});
 		
 		sub.appendChild(node);
